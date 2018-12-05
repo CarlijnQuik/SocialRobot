4629687ffbed4bbfaf7afeb4cd8720d5bf4c22aa
carli <carlijnquik@outlook.com>
2018-12-05 12:51:22 +0000

    printing sentence 1

--- speech_animation.py
+++ speech_animation.py
@@ -5,9 +5,28 @@ import rospy
 import random
 from trash_sentences import *
 from std_msgs.msg import String
-from speechanimationclass import SpeechAnimation
 
 NODE_NAME = 'speech_animation'
+
+class SpeechAnimation(object):
+    def __init__(self):
+        rospy.loginfo('[Movement Manager] Initializing communication with robot.')
+
+        # Movement variables
+        #self.used_sentences = dict()
+        self.sentences = {
+            1:sentence1,
+            2:sentence2,
+            3:sentence3
+        } 
+        rospy.loginfo(self.sentences[1])
+
+        # Face Services and Topics
+        self.moving_robot_pub = rospy.Publisher('speech/speech_demo', String, queue_size=10000)
+
+    def update_behavior(self):
+        # what this module should do
+        rospy.loginfo("test")
 
 # send robot to point received
 # remember that at the end must publish that robot is moving
