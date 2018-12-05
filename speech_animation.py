6486f77a2ff22fd435df1e5a36e4e502bbddf491
carli <carlijnquik@outlook.com>
2018-12-05 11:51:30 +0000

    testing sentences

--- speech_animation.py
+++ speech_animation.py
@@ -3,6 +3,7 @@
 
 import rospy
 import random
+from trash_sentences import *
 from std_msgs.msg import String
 
 NODE_NAME = 'speech_animation'
@@ -14,7 +15,8 @@ class SpeechAnimation(object):
 
         # Movement variables
         self.used_sentences = dict()
-        self.sentences =  # list of sentences by type
+        self.sentences = sentence1 # list of sentences by type
+        rospy.loginfo(sentence1)
 
         # Face Services and Topics
         self.moving_robot_pub = rospy.Publisher('speech/speech_demo', String, queue_size=10000)
@@ -28,15 +30,15 @@ class SpeechAnimation(object):
 # remember that at the end must publish that robot is moving
 
 
-def main():
-    rospy.init_node(NODE_NAME)
-    rospy.loginfo('Starting Node: ' + NODE_NAME)
-    spanim = SpeechAnimation()
-    rospy.loginfo('Movement Manager node running')
-    rate = rospy.Rate(5)
-    while not rospy.is_shutdown():
-        spanim.update_behavior()
-        rate.sleep()
+    def main():
+        rospy.init_node(NODE_NAME)
+        rospy.loginfo('Starting Node: ' + NODE_NAME)
+        spanim = SpeechAnimation()
+        rospy.loginfo('Movement Manager node running')
+        rate = rospy.Rate(5)
+        while not rospy.is_shutdown():
+            spanim.update_behavior()
+            rate.sleep()
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
---
+++ trash_sentences.py
@@ -0,0 +1,3 @@
+sentence1 = "Can you pick this up?"
+sentence2 = "Can you pick the trash?"
+sentence3 = "Why aren't you picking the trash?"
