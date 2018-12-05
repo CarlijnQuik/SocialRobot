90e65c9baaab81c38e5de6e7c9ba2a1bbe387395
carli <carlijnquik@outlook.com>
2018-12-05 12:34:14 +0000

    fixing bug in speech_animation

--- speech_animation.py
+++ speech_animation.py
@@ -5,31 +5,12 @@ import rospy
 import random
 from trash_sentences import *
 from std_msgs.msg import String
+from speechanimationclass import SpeechAnimation
 
 NODE_NAME = 'speech_animation'
 
-
-class SpeechAnimation(object):
-    def __init__(self):
-        rospy.loginfo('[Movement Manager] Initializing communication with robot.')
-
-        # Movement variables
-        self.used_sentences = dict()
-        self.sentences = sentence1 # list of sentences by type
-        rospy.loginfo(sentence1)
-
-        # Face Services and Topics
-        self.moving_robot_pub = rospy.Publisher('speech/speech_demo', String, queue_size=10000)
-
-    def update_behavior(self):
-        # what this module should do
-
-        # TODO
-
 # send robot to point received
 # remember that at the end must publish that robot is moving
-
-
 def main():
     rospy.init_node(NODE_NAME)
     rospy.loginfo('Starting Node: ' + NODE_NAME)
